5fab5ede80be0a3aaa86f333e51d6210d2ad1562
Vera Mariela Reyes Zamora <vera@MacBook-Air-de-Vera.local>
2024-09-15 16:28:23 -0700

    se agregaron graficas

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+import pandas as pd
+import streamlit as st
+import matplotlib as plt
+import plotly.express as px
 
+
+data=pd.read_csv('./vehicles_us.csv')
+
+data.info()
+
+#st.header('Datos de Vehiculos')
+
+#hist_button = st.button('Construir histograma')
+
+#if hist_button:
+    #st.write('Creación de un histograma para los datos de los coches')
+
+    #fig = px.histogram(data, x="odometer")
+
+    #st.plotly_chart(fig, use_container_width=True)
